e18b0a2dce0b08a8b6e4d6553cdd83dc35742608
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-04-30 23:53:43 -0300

    styled components with Diego

---
+++ styled-components/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Title, Button } from "./styles";
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <Title> Hello World </Title>
+          <Button> Click Me </Button>
+        </div>
+      );
+    }
+  }
+
+
+export default App;
---
+++ styled-components/src/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+    color: #F11;`
+
+export const Button = styled.button`
+    color: #000;
+    padding: 20px;
+    `

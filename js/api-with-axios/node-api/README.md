a4e4953594acb47e42f49a03379dc481c152c65e
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2022-02-21 22:22:58 -0300

    Consumindo API no Front-End com Axios

---
+++ js/api-with-axios/node-api/README.md
@@ -0,0 +1,44 @@
+# NodeJS Simple API
+
+<p align="center">
+  <a href="#-tecnologias">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projects</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Lincense</a>
+</p>
+
+
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- JavaScript
+- NodeJS
+- Express
+
+
+
+## 💻 Projeto
+
+Esse projeto foi construído para servir de apoio no estudo de consumo de APIs REST
+
+
+
+## Como utilizar?
+
+1. Baixe esse repositório e com o seu o terminal, entre no diretório
+
+2. Rode o comando `npm install` para instalar as dependências
+
+3. E pra iniciar a aplicação, rode o comando `npm start` 
+
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](.github/LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by Jakeliny Gracielly
---
+++ js/api-with-axios/node-api/index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.listen(5500, () => console.log('Rodando na porta 5500'))
+
+app.use(cors())
+
+app.use(express.json())
+
+let users = [{
+  id: 1,
+  name: "Jakeliny Gracielly",
+  avatar: "https://avatars.githubusercontent.com/u/17316392?v=4",
+  city: "São Paulo"
+}]     
+
+
+app.route('/api').get((req, res) => res.json({
+  users
+}))
+
+app.route('/api/:id').get((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  res.json(user)
+})
+
+app.route('/api').post((req, res) => {
+  const lastId = users[users.length - 1].id
+  users.push({
+    id: lastId + 1,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city
+  })
+  res.json('Saved user')
+})
+
+app.route('/api/:id').put((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city
+  }
+
+  users = users.map(user => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser
+    }
+    return user
+  })
+
+  res.json("Updated user")
+})
+
+app.route('/api/:id').delete((req, res) => {
+  const userId = req.params.id
+
+  users = users.filter(user => Number(user.id) !== Number(userId))
+
+  res.json('Deleted User')
+})
